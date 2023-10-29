a0d5b721cd5f9e6c94ed8bbd41842f82fdba96e7
Jingren-Tang <148117026+Jingren-Tang@users.noreply.github.com>
2023-10-29 15:35:51 +0100

    Add files via upload

--- implementations.py
+++ implementations.py
@@ -3,20 +3,35 @@ import math
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
     Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
 
-    Args:
-        y: The output desired values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        batch_size: The number of samples per batch.
-        num_batches: The total number of batches to generate.
-        shuffle: If True, random shuffle the data before creating batches.
+    Example:
 
-    Yields:
-        minibatch_y: A minibatch of the output values.
-        minibatch_tx: A minibatch of the input data.
+     Number of batches = 9
 
-    This function generates minibatches of the dataset to be used in mini-batch gradient descent algorithms. It ensures
-    that data is optionally shuffled and that batches may overlap if a random offset is applied.
+     Batch size = 7                              Remainder = 3
+     v     v                                         v v
+    |-------|-------|-------|-------|-------|-------|---|
+        0       7       14      21      28      35   max batches = 6
+
+    If shuffle is False, the returned batches are the ones started from the indexes:
+    0, 7, 14, 21, 28, 35, 0, 7, 14
+
+    If shuffle is True, the returned batches start in:
+    7, 28, 14, 35, 14, 0, 21, 28, 7
+
+    To prevent the remainder datapoints from ever being taken into account, each of the shuffled indexes is added a random amount
+    8, 28, 16, 38, 14, 0, 22, 28, 9
+
+    This way batches might overlap, but the returned batches are slightly more representative.
+
+    Disclaimer: To keep this function simple, individual datapoints are not shuffled. For a more random result consider using a batch_size of 1.
+
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
     """
     data_size = len(y)  # NUmber of data points.
     batch_size = min(data_size, batch_size)  # Limit the possible size of the batch.
@@ -45,154 +60,47 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         yield y[start_index:end_index], tx[start_index:end_index]
 
 def calculate_hessian(y, tx, w):
-     """
-    Compute the Hessian matrix for logistic regression.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        w: The current weights, numpy array of shape=(D,)
-
-    Returns:
-        A numpy array of shape=(D,D), representing the Hessian matrix.
-
-    The Hessian matrix is a second-order partial derivative matrix used in optimization problems.
-    """
     _sum = np.zeros((w.shape[0], w.shape[0]))
     for i in range(y.shape[0]):
         _sum = _sum + sigmoid(tx[i, :].dot(w))*(1 - sigmoid(tx[i, :].dot(w)))*tx[i, :].reshape(-1, 1).dot(tx[i, :].reshape(1, -1))
     return _sum/y.shape[0]
 
 def sigmoid(t):
-     """
-    Compute the sigmoid function.
-
-    Args:
-        t: A numpy array or scalar.
-
-    Returns:
-        The sigmoid of t.
-
-    The sigmoid function maps any value to a value between 0 and 1 and is commonly used in logistic regression.
-    """
     return 1/(1 + np.exp(-t))
 
 def compute_lr_gradient(y, tx, w):
-     """
-    Compute the gradient for logistic regression.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        w: The current weights, numpy array of shape=(D,)
-
-    Returns:
-        The gradient as a numpy array of shape=(D,)
-
-    This function calculates the gradient of the logistic regression loss with respect to the weights.
-    """
     return (tx.T.dot(sigmoid(tx.dot(w)) - y))/(y.shape[0])
 
 def compute_lr_loss(y, tx, w):
-     """
-    Compute the logistic regression loss.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        w: The current weights, numpy array of shape=(D,)
-
-    Returns:
-        A scalar, representing the logistic regression loss.
-
-    This function calculates the logistic regression loss, which measures the difference between the predicted and actual values.
-    """
     assert y.shape[0] == tx.shape[0]
     assert tx.shape[1] == w.shape[0]
-    print(tx.dot(w))
     y_star = sigmoid(tx.dot(w))
     _sum = 0
     for i in range(y.shape[0]):
-        print(y_star)
         _sum += (y[i] * math.log(y_star[i]) + (1 - y[i]) * np.log(1 - y_star[i]))
     return - _sum/(y.shape[0])
 
 def compute_mse(y, tx, w):
-     """
-    Compute the Mean Squared Error (MSE) loss.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        w: The current weights, numpy array of shape=(D,)
-
-    Returns:
-        A scalar, representing the MSE loss.
-
-    This function calculates the mean squared error, a common loss function for regression problems.
-    """
     e = y - tx.dot(w)
     return (e.T.dot(e))/(2*y.shape[0])
 
 
 def compute_mae(y, tx, w):
-    """
-    Compute the Mean Absolute Error (MAE) loss.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        w: The current weights, numpy array of shape=(D,)
-
-    Returns:
-        A scalar, representing the MAE loss.
-
-    The MAE loss function calculates the average absolute differences between predicted and actual values.
-    """
-    _sum = 0
+    sum = 0
     for i in range(y.shape[0]):
         if y[i] - tx[i, :].dot(w) >= 0:
-            _sum += y[i] - tx[i, :].dot(w)
+            sum += y[i] - tx[i, :].dot(w)
 
         else:
-            _sum += tx[i, :].dot(w) - y[i]
-    return _sum / (y.shape[0])
+            sum += tx[i, :].dot(w) - y[i]
+    return sum / (y.shape[0])
 
 def compute_gradient_mse(y, tx, w):
-    """
-    Compute the gradient for Mean Squared Error (MSE) loss.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        w: The current weights, numpy array of shape=(D,)
-
-    Returns:
-        The gradient as a numpy array of shape=(D,)
-
-    This function calculates the gradient of the MSE loss with respect to the weights.
-    """
     e = y - tx.dot(w)
     return -tx.dot(e)/(y.shape[0])
 
 
-def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
-     """
-    Apply gradient descent to minimize MSE loss.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        initial_w: Initial weights, numpy array of shape=(D,)
-        max_iters: The maximum number of iterations.
-        gamma: The learning rate.
-
-    Returns:
-        w: The optimized weights.
-        loss: The final loss value.
-
-    This function applies gradient descent to minimize the MSE loss.
-    """
+def mse_gd(y, tx, initial_w, max_iters, gamma):
     w = initial_w
 
     for n_iter in range(max_iters):
@@ -201,70 +109,24 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
         loss = compute_mse(y, tx, w)
     return w, loss
 
-def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
-    """
-    Perform stochastic gradient descent to minimize the Mean Squared Error loss.
-
-    Args:
-        y (numpy array): The output values, with shape (N,).
-        tx (numpy array): The input data, with shape (N, D).
-        initial_w (numpy array): Initial weights, with shape (D,).
-        max_iters (int): Maximum number of iterations.
-        gamma (float): Learning rate.
-
-    Returns:
-        w (numpy array): Optimized weights, with shape (D,).
-        loss (float): Final MSE loss value.
-
-    In each iteration, a single data point is randomly selected to compute the gradient
-    and update the weights, aiming to minimize the MSE loss. This is repeated for max_iters
-    iterations.
-
-    Example:
-        w, loss = mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma)
-    """
+def mse_sgd(y, tx, initial_w, batch_size, max_iters, gamma):
     w = initial_w
 
     for n_iter in range(max_iters):
-        gradient = compute_gradient_mse(y, tx, w)
-        w = w - gamma * gradient
-        loss = compute_mse(y, tx, w)
+        #get minibatch
+        for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
+            stochastic_grad = compute_gradient_mse(minibatch_y, minibatch_tx, w)
+            w = w - gamma * stochastic_grad
+            loss= compute_loss(y,tx,w)
     return w, loss
 
 def least_squares(y, tx):
-    """
-    Solve the least squares problem.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-
-    Returns:
-        w: The solution as a numpy array of shape=(D,)
-        mse: The MSE loss value.
-
-    This function finds the optimal weights that minimize the mean squared error using the normal equations.
-    """
     w = np.linalg.solve(tx.T.dot(tx), tx.T.dot(y))
     e = y-tx.dot(w)
     mse = e.T.dot(e)/(2*y.shape[0])
     return w, mse
 
 def ridge_regression(y, tx, lambda_):
-     """
-    Solve the ridge regression problem.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        lambda_: The regularization parameter.
-
-    Returns:
-        w: The solution as a numpy array of shape=(D,)
-        mse: The MSE loss value with regularization.
-
-    This function finds the optimal weights that minimize the mean squared error with L2 regularization using the normal equations.
-    """
     i = np.identity(tx.shape[1])
     a = tx.T.dot(tx)+2*y.shape[0]*lambda_*i
     b = tx.T.dot(y)
@@ -272,61 +134,18 @@ def ridge_regression(y, tx, lambda_):
     return w, compute_mse(y, tx, w)
 
 
-def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    """
-    Apply gradient descent to minimize logistic regression loss.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        initial_w: Initial weights, numpy array of shape=(D,)
-        max_iters: The maximum number of iterations.
-        gamma: The learning rate.
-
-    Returns:
-        w: The optimized weights.
-        loss: The final logistic regression loss value.
-
-    This function applies gradient descent to minimize the logistic regression loss.
-    """
+def logistic_regression_gd(y, tx, initial_w, max_iters, gamma, lambda_, reg=False):
+    # if reg is set True, penalty will be applied, which means 'Regularized'
     w = initial_w
     for i in range(max_iters):
         gradient = compute_lr_gradient(y, tx, w)
 
+        if reg:
+            gradient = gradient + 2 * lambda_ * w
+
         w = w - gamma * gradient
-        print(w)
-        print(gradient)
-        print(gamma)
+
         loss = compute_lr_loss(y, tx, w)
         print(f"Iteration {i}: Loss = {loss}") 
     return w, loss
 
-def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
-    """
-    Apply gradient descent to minimize regularized logistic regression loss.
-
-    Args:
-        y: The output values, numpy array of shape=(N,)
-        tx: The input data, numpy array of shape=(N,D)
-        lambda_: The regularization parameter.
-        initial_w: Initial weights, numpy array of shape=(D,)
-        max_iters: The maximum number of iterations.
-        gamma: The learning rate.
-
-    Returns:
-        w: The optimized weights.
-        loss: The final regularized logistic regression loss value.
-
-    This function applies gradient descent to minimize the regularized logistic regression loss.
-    """
-    w = initial_w
-    for i in range(max_iters):
-        gradient = compute_lr_gradient(y, tx, w)
-        gradient = gradient + 2 * lambda_ * w
-        w = w - gamma * gradient
-        print(w)
-        print(gradient)
-        print(gamma)
-        loss = compute_lr_loss(y, tx, w)
-        print(f"Iteration {i}: Loss = {loss}") 
-    return w, loss
--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,132 @@
 import numpy as np
 from numpy.linalg import norm
 
+def check_array_shapes(*arrays):
+    for arr in arrays:
+        print(arr.shape)
 
+def analyze_missing_values(tX, y):
+    for col in range(tX.shape[1]):
+        tX_T = np.transpose(tX)
+
+        # Find the positions of missing values in that column
+        null = np.isnan(tX_T[col])
+
+        # Based on the classification result 'y', find two subsets of missing values
+        null_p = np.logical_and(y >= 0, null)  # Subset for positive-classified results
+        null_n = np.logical_and(y < 0, null)   # Subset for negative-classified results
+
+        # Extract the corresponding subsets
+        tX_null = tX[null]         # All rows containing missing values
+        tX_null_p = tX[null_p]     # Rows with positive-classified results and missing values
+        tX_null_n = tX[null_n]     # Rows with negative-classified results and missing values
+
+        # If there are rows containing missing values
+        if (tX_null.shape[0] > 0):
+            # Print the percentage of missing values in that column
+            print('Column', col, 'has {}% missing values'.format(tX_null.shape[0] * 100 / tX.shape[0]))
+            print('P(y = 1|x contains NaN) = {:.3f}%'.format(tX_null_p.shape[0] * 100 / tX_null.shape[0]))
+            print('P(y = -1|x contains NaN) = {:.3f}% \n'.format(tX_null_n.shape[0] * 100 / tX_null.shape[0]))
+
+                                                                                                          
+def replace_values(arr, values_to_replace_with_nan, values_to_replace_with_0_001):
+    for value in values_to_replace_with_nan:
+        arr[arr == value] = np.nan
+
+    for value in values_to_replace_with_0_001:
+        arr[arr == value] = 0.001
+                      
+                                                                                                          
+def remove_columns_with_nan(arr):
+    nan_columns = np.any(np.isnan(arr), axis=0)
+    tX_NoNaN = arr[:, ~nan_columns]
+    return tX_NoNaN                                                                                                          
+
+                                                                                                          
+def perform_pca(X, n_components):
+    """
+    Perform Principal Component Analysis (PCA) on the input data.
+
+    Args:
+    X (numpy.ndarray)
+    n_components (int): The number of principal components to retain.
+
+    Returns:
+    X_reduced (numpy.ndarray): The reduced-dimensional data.
+    """
+    # Center the data 
+    mean = np.mean(X, axis=0)
+    X_centered = X - mean
+    cov_matrix = np.cov(X_centered, rowvar=False)
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+    # Select the top n eigenvectors (principal components)
+    top_eigenvectors = eigenvectors[:, :n_components]
+    # Project the data into the new feature space
+    X_reduced = np.dot(X_centered, top_eigenvectors)
+                                                                                                          
+    return X_reduced
+   
+                                                                                                          
+def k_means_clustering(X_reduced, n_clusters, max_iterations=100):
+    """
+    Perform K-Means clustering on reduced-dimensional data.
+
+    Args:
+    X_reduced (numpy.ndarray):
+    n_clusters (int): The number of clusters.
+    max_iterations (int): Maximum number of iterations.
+
+    Returns:
+    cluster_assignment (numpy.ndarray): An array containing cluster labels for each sample.
+    """
+    np.random.seed(0)
+    initial_centers = X_reduced[np.random.choice(X_reduced.shape[0], n_clusters, replace=False)]
+
+    for iteration in range(max_iterations):
+        # Calculate distances and cluster assignment
+        distances = np.linalg.norm(X_reduced[:, np.newaxis] - initial_centers, axis=2)
+        cluster_assignment = np.argmin(distances, axis=1)
+
+        # Update cluster centers
+        for cluster in range(n_clusters):
+            points_in_cluster = X_reduced[cluster_assignment == cluster]
+            if len(points_in_cluster) > 0:
+                initial_centers[cluster] = np.mean(points_in_cluster, axis=0)
+
+    return cluster_assignment                                                                                                          
+
+def clean_dataset(tX, missing_value_threshold):
+    column_count = tX.shape[1]
+    missing_value_count = np.sum(np.isnan(tX), axis=0)
+    missing_value_ratio = missing_value_count / tX.shape[0]
+    columns_to_remove = np.where(missing_value_ratio > missing_value_threshold)[0]
+    columns_to_keep = np.where(missing_value_ratio <= missing_value_threshold)[0]
+    return columns_to_keep
+
+                           
+def fill_missing_with_median(matrix, cluster_assignment):
+    filled_matrix = matrix.copy()
+    unique_clusters = np.unique(cluster_assignment)
+    column_medians = []
+
+    for cluster in unique_clusters:
+        cluster_indices = np.where(cluster_assignment == cluster)[0]
+        cluster_data = matrix[cluster_indices]
+
+        # Calculate the median for each column in the cluster
+        median = np.nanmedian(cluster_data, axis=0)
+        column_medians.append(median)
+
+        # Find the positions of missing values within the cluster
+        missing_rows, missing_cols = np.where(np.isnan(cluster_data))
+
+        # Fill missing values with the corresponding cluster median
+        for row, col in zip(missing_rows, missing_cols):
+            filled_matrix[cluster_indices[row], col] = median[col]
+
+    return filled_matrix
+                           
+                           
 def standardize(tx):
     """
     Standardize features by mean and standard deviation and return the standardized feature matrix.
@@ -72,37 +197,23 @@ def perform_pca(X, n_components=2):
 
     return X_reduced
 
+def expand_features(x):
+    n, d = x.shape
+    x_expanded = np.c_[np.ones((n, 1)), x]
+    return x_expanded
 
-# def k_means_clustering(X_reduced, n_clusters, max_iterations=100):
-#     """
-#     Perform K-Means clustering on reduced-dimensional data.
-    
-#     Args:
-#     X_reduced (numpy.ndarray): The reduced-dimensional data, where each row represents a sample, and each column represents a reduced feature.
-#     n_clusters (int): The number of clusters.
-#     max_iterations (int): Maximum number of iterations.
+def initialize_w(x_expanded, w_value):
+    num_features = x_expanded.shape[1]
+    initial_w = np.ones(num_features) * w_value
+    return initial_w
 
-#     Returns:
-#     cluster_assignment (numpy.ndarray): An array containing cluster labels for each sample.
-#     """
-#     np.random.seed(0)
-#     initial_centers = X_reduced[np.random.choice(X_reduced.shape[0], n_clusters, replace=False)
-                                
-                            
-#     for iteration in range(max_iterations):
-#         distances = np.linalg.norm(X_reduced[:, np.newaxis] - initial_centers, axis=2)
-#         cluster_assignment = np.argmin(distances, axis=1)
-        
-#         for cluster in range(n_clusters):
-#             points_in_cluster = X_reduced[cluster_assignment == cluster]
-#             if len(points_in_cluster) > 0:
-#                 initial_centers[cluster] = np.mean(points_in_cluster, axis=0)
-    
-#     return cluster_assignment
+def setup_features_and_weights(x, w_value):
+    x_expanded = expand_features(x)
+    initial_w = initialize_w(x_expanded, w_value)
+    return x_expanded, initial_w
 
-
-                                
-                                
+                           
+                           
 # def random_forest():
 #     """
 #     Args:
@@ -153,6 +264,7 @@ def perform_pca(X, n_components=2):
     
     
 #     return final_prediction
+
 
 
 
